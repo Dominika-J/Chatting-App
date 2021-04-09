5271f4fc2ad7e333f2f67ff338a3cf70e74c1243
Dominika <dominika.jansova1011@gmail.com>
2021-04-09 10:04:38 +0200

    chatengine data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+
 import './App.css';
 
 const App = () => {
     return (
         <ChatEngine
             height="100vh"
-            projectID=""
-            userName=""
-            userSecret=""
-        
+            projectID="9a567ff2-2e52-4854-bd1a-f321c3bacb4c"
+            userName="ReactUser"
+            userSecret="123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
         />
     );
 }
